ffd86129306040fd8b6957c481b410ab8b89bc36
よね/Yone <git@yoneyo.com>
2025-12-21 22:42:21 +0900

    Release: Version 1.2.0

--- src/pages/app/scripts/ssc-web/ssc-web.js
+++ src/pages/app/scripts/ssc-web/ssc-web.js
@@ -27,7 +27,7 @@ export class SSCWeb {
      * 
      * @type {Version}
      */
-    static VERSION = new Version(1, 1, 0, Version.levels.stable);
+    static VERSION = new Version(1, 2, 0, Version.levels.stable);
 
     /**
      * アプリケーション名
