e87c57cf82df4f8cbe3f306c22aadbfc2cf66c88
よね/Yone <git@yoneyo.com>
2025-12-21 16:48:02 +0900

    Feature(Map): Set `attributionControl` to false

--- src/pages/app/scripts/modules/maps/map.js
+++ src/pages/app/scripts/modules/maps/map.js
@@ -42,6 +42,7 @@ export class Map {
      */
     async initialize() {
         this.map = L.map('map', {
+            attributionControl: false,
             center: Map.DEFAULT_LAT_LNG,
             zoom: Map.DEFAULT_ZOOM,
             minZoom: 4,
