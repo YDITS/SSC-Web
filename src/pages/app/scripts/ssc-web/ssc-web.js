8179077937764587cc90730b9e06a7526e5f0895
よね/Yone <git@yoneyo.com>
2025-12-21 23:30:07 +0900

    Fix(app): メニューのバージョン表示 `Ver` のタイポを修正

--- src/pages/app/scripts/ssc-web/ssc-web.js
+++ src/pages/app/scripts/ssc-web/ssc-web.js
@@ -96,7 +96,7 @@ export class SSCWeb {
             this.#enableDebugMode();
         }
 
-        document.getElementById("menu-version").textContent = `Vers ${SSCWeb.VERSION.string}`;
+        document.getElementById("menu-version").textContent = `Ver ${SSCWeb.VERSION.string}`;
         document.getElementById("expandInformationDetailsButton").addEventListener("click", () => this.#onClickInformationDetailsButton());
         const modals = document.getElementById("modals");
 
